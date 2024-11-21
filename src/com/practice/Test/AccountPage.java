13c3579db463bd61bc45378c4f00154b235aa2a4
unknown <piyush.patil1333@gmail.com>
2024-11-21 17:53:25 +0530

    Second commit

---
+++ src/com/practice/Test/AccountPage.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.practice.Test;
+
+/**
+ * @author HP
+ *
+ */
+public class AccountPage {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+      System.out.println("Addition of Account Page");
+	}
+
+}
--- src/com/practice/pages/HomePage.java
+++ src/com/practice/pages/HomePage.java
@@ -4,7 +4,7 @@ public class HomePage {
 
 	public static void main(String[] args) {
 	   System.out.println("This is home Page");
-
+	   System.out.println("This is home Page 2nd commit");
 	}
 
 }
